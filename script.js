b7ef46b38b77429614923f3b88094b36e83d4543
Adilson Dias <adilsondias@outlook.com>
2020-09-12 23:17:20 -0300

    Esboço jogo papel pedra tesoura

---
+++ script.js
@@ -0,0 +1,12 @@
+// gerar nunero aleatorios para o pc pegar
+
+
+
+
+// gerar numeros aletoros para o jogara 
+
+// relacionar os numeros do elementos
+
+// forma do usuario escolher
+
+// resulado de quem ganhou ou deu empate
