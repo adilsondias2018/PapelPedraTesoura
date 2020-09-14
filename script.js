e28e0031308ecfb8862d270cb001d589f231165c
Adilson Dias <adilsondias@outlook.com>
2020-09-13 22:59:08 -0300

    Recebendo dados enviandos pelo usuario e gerados pelo computador ok

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // gerar nunero aleatorios para o pc pegar
 
+let computador = Math.floor( Math.random() *3 +1)
+console.log(computador)
+
+// receber numero informador pelo usuários
+
+const button = document.getElementById("armazeneButton")
+button.onclick = function () {
+   let numeroJogador = parseInt ( document.getElementById("input").value)
+    let computador = Math.floor( Math.random() *3 +1)
+
+
+    console.log(computador, numeroJogador)
+    analiseResultado(computador, numeroJogador)
+}
+
+
+function analiseResultado( ){
+
+
+}
+
+
+// Quais as formas de inputs existentes no JavaScript
+
+/*
+    metodo input
+
+    
+
+*/
+
+
 
 
 
