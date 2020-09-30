ade87a8daf11f7e510a5373a954a3c44f2e94bfb
Adilson Dias <adilsondias@outlook.com>
2020-09-29 21:31:55 -0300

    Ainda falta ajustes nas chamadas da funçao

--- script.js
+++ script.js
@@ -12,95 +12,83 @@ let pontuacaoEmpate = 0
 
 let resultado = document.createElement('h3')
 let pontuacao = document.createElement('h3')
-
- pontuacao.textContent = pontuacaoComputador
- document.getElementById('pontuacaoComputador').appendChild(pontuacao)
+let escolhaJogador   = document.createElement('h1')
+let escolhaComputador   = document.createElement('h1')
 
     for (let i = 0; i < figuras.length; i++) {
-
-        console.log ('Fora event click acima ' +   pontuacaoComputador, pontuacaoEmpate, pontuacaoJogador);
-
-
+      
         figuras[i].addEventListener('click', (e) => {
             const computador = Math.floor(Math.random() * 3 + 1)
-            const escolhaUsario = parseInt(figuras[i].id)
-            console.log(computador, escolhaUsario)      
-                              
+            const escolhaUsario = parseInt(figuras[i].id) 
 
             if ((computador === 1 && escolhaUsario === 3) || (computador === 3 && escolhaUsario === 2) || (computador === 2 && escolhaUsario === 1)) {
-
                
+                escolhasJogadorComputador(escolhaUsario, computador)
+                
                
                 resultado.innerText = ' O computador venceu !';
 
-                document.getElementById('resultado').appendChild(resultado)
-                
+                document.getElementById('resultado').appendChild(resultado)                
 
                 pontuacaoComputador += 1
 
                 console.log(computador, escolhaUsario, pontuacaoComputador)
 
-                // contadorComputador(pontuacaoComputador)
-
-
-
-                
+                contadorComputador(pontuacaoComputador)
 
             } else if ((computador === 3 && escolhaUsario === 1) || (computador === 2 && escolhaUsario === 3) || (computador === 1 && escolhaUsario === 2)){
 
+                escolhasJogadorComputador(escolhaUsario, computador)
+               
                 resultado.innerText = ' O usuário venceu';
                 document.getElementById('resultado').appendChild(resultado)
                 console.log(computador, escolhaUsario)
                 pontuacaoJogador += 1
                 console.log(computador, escolhaUsario, pontuacaoJogador)                
-                // contadorJogagor(pontuacaoJogador)               
+                contadorJogagor(pontuacaoJogador)               
 
             }else if ( computador === escolhaUsario){
 
+                escolhasJogadorComputador(escolhaUsario, computador)
                 resultado.innerText = ' Empate';
                 document.getElementById('resultado').appendChild(resultado)
                 pontuacaoEmpate += 1
-                // contadorEmpate(pontuacaoEmpate)
+                contadorEmpate(pontuacaoEmpate)
             }
-
-
-               
-
-            console.log ('Final for ' +   pontuacaoComputador, pontuacaoEmpate, pontuacaoJogador);
-
             
         })
-
-        console.log ('Fora event click abaixo ' +   pontuacaoComputador, pontuacaoEmpate, pontuacaoJogador);
-
-      
-            
-            
+     
     }
 
 function contadorEmpate(pontosAtuis) {
 
     pontuacao.innerText = pontosAtuis
-
     document.getElementById('pontuacaoEmpate').appendChild(pontuacao)
 }
 
 function contadorJogagor(pontosAtuis) {
 
     pontuacao.innerText = pontosAtuis
-
     document.getElementById('pontuacaoJogador').appendChild(pontuacao)
 }
 
 function contadorComputador(pontosAtuis) {
 
     pontuacao.innerText = pontosAtuis
-
     document.getElementById('pontuacaoComputador').appendChild(pontuacao)
 }
 
-contadorEmpate(pontuacaoEmpate)
-contadorJogagor(pontuacaoJogador)
-contadorComputador(pontuacaoComputador)  
+function escolhasJogadorComputador(jogador, computador){
+
+                 escolhaJogador.innerText = jogador
+                 escolhaComputador.innerText = computador
+                document.getElementById('jogador').appendChild(escolhaJogador)
+                document.getElementById('computador').appendChild(escolhaComputador)
+
+
+}
+
+
+
 
 
